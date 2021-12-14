2bccb7b5a8c222532afc7af74d05c3559866b878
Matt Eva <matteweva@gmail.com>
2021-12-14 12:00:29 -0800

    commit tree 395f4158c8ec3af1b3ce31d4cff43416ad1e5e89

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "is not included in the list"}
+    validate :is_clickbait
+
+    def is_clickbait
+        t = title.to_s
+        # if t.include? "True Facts"  
+        #     errors.add(:title, "Title must be clickbait!")
+        # end
+        bait_array = ["Won't Believe", "Secret", "Top", "Guess"]
+        unless bait_array.any? {|bait| t.include? bait}
+            errors.add(:title, "Title must be clickbait!")
+        end
+    end
+
 end
